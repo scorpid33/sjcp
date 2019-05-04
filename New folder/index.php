acba9553a6dfb0f2ea9a23fdcb4c0074ccbfd3fa
LabVaKars <34735460+LabVaKars@users.noreply.github.com>
2019-05-04 23:11:14 +0300

    Add files via upload

---
+++ New folder/index.php
@@ -0,0 +1,144 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Dashboard Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="..//css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../css/dashboard.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Dashboard</a></li>
+            <li><a href="#">Settings</a></li>
+            <li><a href="#">Profile</a></li>
+            <li><a href="#">Help</a></li>
+          </ul>
+          <form class="navbar-form navbar-right">
+            <input type="text" class="form-control" placeholder="Search...">
+          </form>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">Reports</a></li>
+            <li><a href="#">Analytics</a></li>
+            <li><a href="#">Export</a></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+            <li><a href="">Nav item</a></li>
+            <li><a href="">Nav item again</a></li>
+            <li><a href="">One more nav</a></li>
+            <li><a href="">Another nav item</a></li>
+            <li><a href="">More navigation</a></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+            <li><a href="">Nav item again</a></li>
+            <li><a href="">One more nav</a></li>
+            <li><a href="">Another nav item</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          <h1 class="page-header">Dashboard</h1>
+
+          <div class="table-responsive">
+            <table class="table table-striped">
+				  <form method="post" action="add_quiz.php">
+					<div class="form-group">
+					  <label for="text">Question</label>
+					  <input type="text" class="form-control" name="op1"  placeholder="Enter question">
+					</div>
+
+					<div class="form-group">
+					  <label for="text">option-1</label>
+					  <input type="text" class="form-control"  name="op1"  placeholder="Enter option-1">
+					</div>
+					<div class="form-group">
+					  <label for="text">option-2</label>
+					  <input type="text" class="form-control" name="op2"  placeholder="Enter option-2">
+					</div>
+					
+					<div class="form-group">
+					  <label for="text">option-3</label>
+					  <input type="text" class="form-control"  name="op3"  placeholder="Enter option-3">
+					</div>
+					
+					<div class="form-group">
+					  <label for="text">option-4</label>
+					  <input type="text" class="form-control"  name="op4" id="email" placeholder="Enter option-4">
+					</div>
+					<div class="form-group">
+					  <label for="text">answer</label>
+					  <input type="text" class="form-control" name="ans" id="email" placeholder="Enter answer">
+					</div>
+						<div class="form-group">
+						   
+						   <select class="form-control" id="sel1" name="cat">
+								<option value="" >choose category</option>
+								<option value="1">html</option>
+						  </select>
+						</div>
+					<button type="submit" class="btn btn-default">Submit</button><br>
+				  </form>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="../js/vendor/holder.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
---
+++ answer.php
@@ -0,0 +1,79 @@
+<?php
+include("class/users.php");
+$ans=new  users;
+$answer=$ans->answer($_POST);
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>answer</title>
+	  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  
+</head>
+<body>
+	<center>
+	<?php
+	$total_qus=$answer['right']+$answer['wrong']+$answer['no_answer'];
+	$attempt_qus=$answer['right']+$answer['wrong'];
+	?>
+	<div class="container">
+	<div class="col-sm-2"></div>
+	<div class="col-sm-8">
+  <h2>Your test results</h2>
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>Total number of Questions</th>
+        <th><?php echo $total_qus; ?></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Attempted questions</td>
+        <td><?php echo $attempt_qus;?></td>
+      </tr>
+      <tr>
+        <td>Right answer </td>
+        <td><?php echo $answer['right'];?></td>
+      </tr>
+      <tr>
+        <td>Wrong answer</td>
+        <td><?php echo $answer['wrong'];?></td>
+      </tr>
+	  <tr>
+        <td>No answer</td>
+        <td><?php echo $answer['no_answer'];?></td>
+      </tr>
+	  <tr>
+        <td>Your result</td>
+        <td><?php 
+		$per=($answer['right']*100)/($total_qus);
+		
+		echo $per."%";
+		?></td>
+      </tr>
+    </tbody>
+  </table></div>
+  <div class="col-sm-2"></div>
+</div>
+	    <div class="button-panel">
+    <p><a href="home2.php">Back to Home</a></p>
+    </div>
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	</center>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</body>
+</html>
