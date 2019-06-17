b42c5b81418d3cf1f95998dc7a883be61e997afa
Niklavs Emils Eglitis <34540352+NiklavsE@users.noreply.github.com>
2019-06-18 00:55:28 +0300

    Update TravelGroupListView.php

--- TravelGroup/TravelGroupListView.php
+++ TravelGroup/TravelGroupListView.php
@@ -69,31 +69,15 @@ require_once('../class/TravelGroupRepository.php');
           <div class="table-responsive">
             <table class="table table-striped">
           </div>
-    <form method="post">
-          <?php
-            /*$travel_group_list_item = new TravelGroupRepository; 
-            foreach ($travel_group_list_item->getTravelGroupList() as $travel_group) { 
-                echo "<tr>";
-                echo "<td>".$travel_group->place."</td>";
-                echo "<td align='center'>";
-                echo "<a href='TravelGroupListItemView.php'  title='View Record' data-toggle='tooltip'><input type='submit' class='btn btn-primary' value='View'></a>  ";
-                echo "<a href='TravelGroupListItemView.php'  title='Edit Record' data-toggle='tooltip'><input type='submit' class='btn btn-info' value='Edit'></a>";
-                echo "</td><td align='center'>";
-                echo "<form method='POST'  onsubmit='return confirm('Do you really want to submit the form?');'>
-                <input type='hidden' name='id' value=".$travel_group->description."></input><input type='submit' class='btn btn-danger' name='Delete_record' value='Delete' >";
-                echo '</td></tr>';
-            }*/
-          ?>
                           <tr>
                 <td>Spānija</td>
                 <td align='center'>
-                <a href='TravelGroupListItemView.php'  title='View Record' data-toggle='tooltip'><input type='submit' class='btn btn-primary' value='View'></a>
+                <a href='TravelGroupListItemView.php'  title='View Record' data-toggle='tooltip'><input type='button' class='btn btn-primary' value='View'></a>
                 <a href='TravelGroupListItemView.php'  title='Edit Record' data-toggle='tooltip'><input type='submit' class='btn btn-info' value='Edit'></a>
                 </td><td align='center'>
                 <form method='POST'  onsubmit='return confirm('Do you really want to submit the form?');'>
                 <input type='hidden' name='id' value="Ceļojums kaut kur"></input><input type='submit' class='btn btn-danger' name='Delete_record' value='Delete' >
                 </td></tr>
-        </form>
             </tbody>
             </table>
           </div>
