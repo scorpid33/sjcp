875bafa60b85cbd799cc9ddabf22bf83add0d6d5
Erdino-Sviests <38459653+Erdino-Sviests@users.noreply.github.com>
2019-06-20 03:46:30 +0300

    Update blog_view.php

--- blog_view.php
+++ blog_view.php
@@ -18,19 +18,39 @@ exit(); }
 	
 	
 	$result_row = $query_result->fetch_assoc();
+	
+	
 
 ?>
 
 <body>
-<div class="container">
-
+<div class="fixed-header">
+<div style="text-align: left; margin-left: 15px;
+  position: absolute;
+  top: 50%;
+  -ms-transform: translateY(-50%);
+  transform: translateY(-50%);">
+<a href="?link=1">Atgriezties atpakaļ</a>
+</div>
+<div style="text-align: center;">
 <?php
-echo "<h1>".$result_row['title']."</h1>";
+echo "<h1>Blogs : ".$result_row['title']."</h1></div></div>";
+?>
+<div style="margin-top: 25px;" class="container">
+<?php
 if (!empty($result_row['images_folder_link'])) {
-	echo "<br><img src='".$result_row['images_folder_link']."' alt='Blog image'>";
+	echo "<br><img src='".$result_row['images_folder_link']."' class='blog_image_fit' alt='Blog image'>";
 }
 echo "<br><br><br>".$result_row['text'];
 ?>
 </div>
 </body>
-<?php $con->close(); ?>
+<?php 
+if(isset($_GET['link'])){
+$link=$_GET['link'];
+       if ($link == '1'){
+            header("Location: home2.php");
+		exit();
+       }
+}
+	   $con->close(); ?>
