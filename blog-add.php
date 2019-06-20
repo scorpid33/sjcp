d39f0734577404916d4abb38c4115d070307ddb0
Erdino-Sviests <38459653+Erdino-Sviests@users.noreply.github.com>
2019-06-20 03:46:14 +0300

    Update blog-add.php

--- blog-add.php
+++ blog-add.php
@@ -1,68 +1,40 @@
 <?php
-include_once('header.php');
-require('dbcon.php');
-//Test if logged in
-if(!isset($_SESSION["username"])){
-header("Location: index.php");
-exit(); }
 
-
-  // If upload button is clicked ...
-  if (isset($_POST['submit_blog'])) {
-  	// Get image name
-	$image = "";
-  	$image = $_FILES['image_upload_text']['name'];
-  	// Get text
-  	$blog_content = mysqli_real_escape_string($con, $_POST['blog_content']);
-	$blog_title = mysqli_real_escape_string($con, $_POST['blog_title']);
-
-  	// image file directory
-	if (!empty($image)) {
-  	$target = "images/".basename($image);
-	} else {
-		$target = null;
-	}
-	/* ID from table users can change. Need final db */
+function get_user_blog_list() {
+    
 	if(!isset($_SESSION['user_id'])) {
 		include_once("get_user_id.php");
 	}
 	
-  	$sql = "INSERT INTO blogs (users_id, images_folder_link, title, text) VALUES (".$_SESSION['user_id'].", '$target','$blog_title' , '$blog_content')";
-  	// execute query
-  	// mysqli_query($con, $sql);
-
-  if (move_uploaded_file($_FILES['image_upload_text']['tmp_name'], $target)) {
-		//Success
-  	}else{
-  		$msg = "Failed to upload image";
-  	}
+	if (!isset($con)) {
+		$con = mysqli_connect("localhost","root","","sjcp");
+		
+		// Check connection
+		if (mysqli_connect_errno())
+		{
+			echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		}
+	}
 	
-	if (!$con->query($sql) === TRUE) {
-		echo "Error: " . $sql . "<br>" . $con->error;
+	$sql = "SELECT *  FROM blogs WHERE users_id='".$_SESSION["user_id"]."'";
+	
+	$query_result = mysqli_query($con, $sql);
+	
+	
+	while ($row = $query_result->fetch_assoc()) {
+        $user_blogs [] = $row;
+    }
+
+    /* free result set */
+    $query_result->free();
+	
+	return $user_blogs;
 }
-  }
-  $result = mysqli_query($con, "SELECT * FROM blogs");
+	$user_blog_array = get_user_blog_list();
+	
+	
+	foreach ($user_blog_array as $row) {
+		Echo "<a href='blog_view.php?id=".$row['blog_id']."'><div class='blog_list blog-border'><img src='".$row['images_folder_link']."' width='150' height='145' alt='Blog image'> <label>".$row['title']."</label></div></a>";
+	}
 
 ?>
-
-<body>
-<div class="container">
-<h2> Bloga Izveide </h2>
-
-<form method="POST" enctype="multipart/form-data">
-<h2>Virsraksts</h2>
-<textarea rows="1" cols="35" name="blog_title" class="h1"></textarea>
-<h2><label for="Img_file_upload">Augšuplādēt failu</label></h2>
-
-<!–– for iekš label pasaka kādu nosaukumu dot failam ar noteiktu id. Izmanto style="visibility:hidden;", lai paslēptu pogu, un tādā veidā lebel strādātu kā poga. --->
-
-<input type="file" name="image_upload_text" id="Img_file_upload" multiple accept='image/*' style="visibility:hidden;" >
-
-<h2>Raksts</h2>
-<textarea rows="20" cols="120" name="blog_content"></textarea>
-<br><br>
-<input type="submit" name="submit_blog" value="Izveidot blogu">
-</form>
-</div>
-</body>
-<?php $con->close(); ?>
